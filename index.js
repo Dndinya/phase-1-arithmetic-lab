09460f9a637a0d31d3e2293ec212d363222e108c
Derrick <derrick.ndinya@student.moringaschool.com>
2023-12-12 00:42:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+let random = 0;
+random++;
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+console.log(mod);
+
+let max = Math.max(1,2,3,4,5,7,8,9,10,12,19,20);
+console.log(math);
